691a5d7d9933a34a669be5687447182f0d12eb67
Ziting Shen <zshen@brynmawr.edu>
2018-03-18 22:56:33 -0400

    basic sjf test

--- hw4/ziting/sjf_basic.c
+++ hw4/ziting/sjf_basic.c
@@ -12,7 +12,7 @@ int main(void) {
   char *hello_str = "Hello, world!";
   int tid_1 = thread_create(hello, hello_str, 0);
 
-  printf("Simplest test case for FIFO.\n");
+  printf("Simplest test case for SJF.\n");
   printf("Print \"Hello, world!\" on success.\n");
 
   if (thread_join(tid_1) < 0) exit(EXIT_FAILURE);
