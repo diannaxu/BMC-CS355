777a267a60ce61a32e70c9f61647463fae2aa05d
Dylan Slack <dylanzslack@gmail.com>
2018-03-20 15:20:22 -0400

    this test was incorrect

--- hw4/dylan/misuses.c
+++ hw4/dylan/misuses.c
@@ -13,25 +13,37 @@ int main(void) {
   printf("This assumes that the priority queue range is from -1 to 1\n");
   printf("because it tests adding a thread with priority 100\n");
   printf("On success, it prints 'Woo!', does not crash, and does not cause any memory leaks\n"); 
-  
-  int tidx = thread_create(foo, NULL, 0);
-  if (tidx != -1) 
-  	exit(EXIT_FAILURE);
 
+  int tidx = thread_create(FIFO, NULL, 0);
+  if (tidx != -1) 
+    exit(EXIT_FAILURE);
+  
+  // thread lib terminate could return success or failure,
+  // just make sure no memory leaks or crashes occur
+  thread_libterminate();
   thread_libterminate();
 
-  if (thread_libinit(PRIORITY) == FAILURE)
+  if (thread_libinit(FIFO) == FAILURE) {
     exit(EXIT_FAILURE);
+  }
 
-  int tid1 = thread_create(foo, NULL, 100);
-  if (tid1 == -1)
-  	exit(EXIT_FAILURE);
-
-  if (thread_join(tid1) == -1)
-  	exit(EXIT_FAILURE);
-
-  if (thread_libterminate() == FAILURE)
+  int tid1 = thread_create(fifo_misuse_foo, NULL, 100);
+  if (tid1 != FAILURE) {
     exit(EXIT_FAILURE);
+  }
+
+  tid1 = thread_create(fifo_misuse_foo, NULL, -1);
+  if (tid1 == FAILURE) {
+    exit(EXIT_FAILURE);
+  }
+
+  if (thread_join(tid1) == FAILURE) {
+    exit(EXIT_FAILURE);
+  }
+
+  if (thread_libterminate() == FAILURE) {
+    exit(EXIT_FAILURE);
+  }
 
   exit(EXIT_SUCCESS);
 }
