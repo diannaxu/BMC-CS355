e06e42dfe02bcaa137c96dcd068202a9b9698f1c
Ziting Shen <zitingshenbmc@gmail.com>
2018-03-31 17:56:22 -0400

    basic allocate and free test

---
+++ hw5/ziting/basic.c
@@ -0,0 +1,44 @@
+#include "mem.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <assert.h>
+#include <limits.h>
+#include <unistd.h>
+
+#define NUM_ALLOC 5
+#define FREE_FREQ 100          // 1:100
+#define COALESCE_FREQ 100000   // 1:100000
+#define BYTE 8
+
+clock_t begin, end;
+
+static void print_execution_time(clock_t begin, clock_t end) {
+  double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+  printf("Execution time: %.2f seconds\n", time_spent);
+}
+
+int main() {
+  int result = Mem_Init(NUM_ALLOC * 40);
+  assert(result == 0);
+
+  void **ptrs = malloc(sizeof(void*) * NUM_ALLOC);
+
+  for (int i = 0; i < NUM_ALLOC; i++) {
+    ptrs[i] = Mem_Alloc(BYTE);
+    assert(ptrs[i] != NULL);
+
+    Mem_Dump();
+  }
+
+  for (int i = 0; i < NUM_ALLOC; i++) {
+    Mem_Free(ptrs[i], 0);
+
+    Mem_Dump();
+  }
+  end = clock();
+  print_execution_time(begin, end);
+  free(ptrs);
+
+  return EXIT_SUCCESS;
+}
