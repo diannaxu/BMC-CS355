43986357e49cfc8390914136cf4e4bdea6c59c6d
Sarah Depew <star.1776@gmail.com>
2018-04-01 14:16:13 -0400

    Pushing Tests

---
+++ hw5/Mem Tests/Test1.c
@@ -0,0 +1,44 @@
+//
+// Created by Sarah Depew on 3/26/18.
+//
+
+#include "../../mem.h"
+#include "../../helper.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NUM_ALLOC 7
+
+/*
+ * Testing for 8-byte alignment
+ * Expected Behavior: Should see 8-byte alignment on dump printout
+ */
+int test_one() {
+    int result = Mem_Init(NUM_ALLOC);
+    if(result == ERROR) {
+        return EXIT_FAILURE;
+    } else {
+        Mem_Dump();
+    }
+
+    void *resultPtr = Mem_Alloc(NUM_ALLOC);
+    if(resultPtr == NULL) {
+        return EXIT_FAILURE;
+    } else {
+        Mem_Dump();
+    }
+
+    result = Mem_Free(resultPtr, 0);
+    if(result == ERROR) {
+        return EXIT_FAILURE;
+    } else {
+        Mem_Dump();
+    }
+
+    return EXIT_SUCCESS;
+}
+
+
+int main() {
+    exit(test_one());
+}
