efff6116ef82b279a2bb5093afe8dde96fbbddab
Ziting Shen <zitingshenbmc@gmail.com>
2018-04-01 00:14:32 -0400

    odd-sized allocations

---
+++ hw5/ziting/simple_odd.c
@@ -0,0 +1,42 @@
+#include "mem.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <assert.h>
+#include <limits.h>
+#include <unistd.h>
+
+#define NUM_ALLOC 5
+#define SIZE_ALLOC 11
+
+clock_t begin, end;
+
+static void print_execution_time(clock_t begin, clock_t end) {
+  double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+  printf("Execution time: %.2f seconds\n", time_spent);
+}
+
+int main() {
+  int result = Mem_Init(NUM_ALLOC * 40);
+  assert(result == 0);
+
+  void **ptrs = malloc(sizeof(void*) * NUM_ALLOC);
+
+  for (int i = 0; i < NUM_ALLOC; i++) {
+    ptrs[i] = Mem_Alloc(SIZE_ALLOC);
+    assert(ptrs[i] != NULL);
+
+    Mem_Dump();
+  }
+
+  for (int i = 0; i < NUM_ALLOC; i++) {
+    Mem_Free(ptrs[i], 0);
+
+    Mem_Dump();
+  }
+  end = clock();
+  print_execution_time(begin, end);
+  free(ptrs);
+
+  return EXIT_SUCCESS;
+}
