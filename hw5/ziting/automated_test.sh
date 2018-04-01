6377142e4fd5734a81e3825019d98e3ad8405257
Ziting Shen <zitingshenbmc@gmail.com>
2018-04-01 16:41:41 -0400

    add shell script for testing

--- hw5/ziting/automated_test.sh
+++ hw5/ziting/automated_test.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 make
-./mem_init_misuse
-./simple_8_byte
-./simple_8_byte_coalesce
-./simple_aligned simple_odd
-./mem_init_1_page
-./mem_init_1_page_roundup
-./free_null_pointer
-./memory_writable
-./allocate_all_memory 
-./allocate_too_much worst_fit
+./mem_init_misuse >> log.txt
+./simple_8_byte >> log.txt
+./simple_8_byte_coalesce >> log.txt
+./simple_aligned simple_odd >> log.txt
+./mem_init_1_page >> log.txt
+./mem_init_1_page_roundup >> log.txt
+./free_null_pointer >> log.txt
+./memory_writable >> log.txt
+./allocate_all_memory >> log.txt
+./allocate_too_much worst_fit >> log.txt
+grep fail < log.txt
