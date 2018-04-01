5f7b112747f058339eab25c442b58a56c9f7643c
Ziting Shen <zitingshenbmc@gmail.com>
2018-04-01 16:13:23 -0400

    test allocating too much

--- hw5/ziting/allocate_all_memory.c
+++ hw5/ziting/allocate_all_memory.c
@@ -24,11 +24,11 @@ int main() {
 
   printf("--start allocating (assumed the header size is %d bytes).\n", HEADER_SIZE);
   ptr = Mem_Alloc(getpagesize()/2-HEADER_SIZE);
-  ptr2 = Mem_Alloc(getpagesize()/2-HEADER_SIZE);
   assert(ptr != NULL);
   assert(((long) ptr) % 8 == 0);
   Mem_Dump();
 
+  ptr2 = Mem_Alloc(getpagesize()/2-HEADER_SIZE);
   assert(ptr2 != NULL);
   assert(((long) ptr2) % 8 == 0);
   Mem_Dump();
