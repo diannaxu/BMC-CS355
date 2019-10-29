fc322eaa13c0cd9e0ea7c62b68e3eeaeb88ce6e3
Ruby M <ranmi.chaos.fox@gmail.com>
2019-10-29 10:55:34 -0400

    Adding additional tests

--- hw4/rmalusa/misuse_test_2.c
+++ hw4/rmalusa/misuse_test_2.c
@@ -1,6 +1,6 @@
-// MISUSE_TEST_2.c
-// Tests that the library reacts reasonably when the user tries to 1) create and join threads before initializing it and 2) join an invalid TID 
-// Expected behavior: return -1 for each operation, causing the error messages to print; no segfaults/memory leaks 
+// Author: Ruby Malusa
+// MISUSE_TEST_2.C
+// Tests that the library reacts reasonably to things a user might do that are wrong, like performing thread operations without initializing the library, initializing the library twice without terminating it in between, and terminating the library twice
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,23 +15,43 @@ int main(){
   // Thread creation without initializing library 
   int tid = thread_create(blank, NULL, 0);
   if(tid == ERROR){
-    printf("Thread could not be created\n");
+    printf("Thread could not be created (expected: library not initialized)\n");
   }
 
   // Thread joining without initializing library 
   int status = thread_join(tid);
   if(status == ERROR){
-    printf("Thread could not be joined\n");
+    printf("Thread could not be joined (expected: library not initialized)\n");
   }
 
   // Initialize library 
-  thread_libinit(FIFO);
-
+  status = thread_libinit(FIFO);
+  if(status == ERROR){
+    printf("Library could not be initialized\n");
+    exit(EXIT_FAILURE); 
+  }
+  
+  // Initialize library a second time
+  status = thread_libinit(SJF); 
+  if(status == ERROR){
+    printf("Library could not be initialized (expected: library already initialized)\n"); 
+  }
+  
   // Thread joining with an invalid TID
   status = thread_join(ERROR);
   if(status == ERROR){
-    printf("Thread could not be joined\n"); 
+    printf("Thread could not be joined (expected: TID invalid)\n"); 
   }
 
-  thread_libterminate(); 
+  status = thread_libterminate();
+  if(status == ERROR){
+    printf("Library could not be terminated\n");
+    exit(EXIT_FAILURE); 
+  }
+  
+  status = thread_libterminate();
+  if(status == ERROR){
+    printf("Library could not be terminated (expected: already terminated)\n");
+  }
+  exit(EXIT_SUCCESS);
 }
