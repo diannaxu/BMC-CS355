008643739c8d5f922851af547ed9de7b4bb14389
Yuxiao Wang <me@rayrw.com>
2019-11-10 22:30:51 -0500

    add basic test suite

---
+++ hw5/ywang13/align.c
@@ -0,0 +1,33 @@
+//
+// Created by Yuxiao Wang on 11/10/19.
+// test simple allocation and alignment
+//
+
+#include <assert.h>
+#include <stdlib.h>
+#include "mem.h"
+
+int main() {
+    assert(Mem_Init(1) == 0);
+
+    // 8-byte alignment
+    void* p1 = Mem_Alloc(1);
+    assert(p1);
+
+    // 8-byte allocation
+    void* p2 = Mem_Alloc(8);
+    assert(p2);
+    void* p3 = Mem_Alloc(8);
+    assert(p3);
+
+    // some other sized allocation
+    void* p4 = Mem_Alloc(16);
+    assert(p4);
+    void* p5 = Mem_Alloc(32);
+    assert(p5);
+    void* p6 = Mem_Alloc(25);
+    assert(p6);
+
+    Mem_Dump();
+    exit(EXIT_SUCCESS);
+}
---
+++ hw5/ywang13/exhaust_space.c
@@ -0,0 +1,23 @@
+//
+// Created by Yuxiao Wang on 11/10/19.
+// request allocation until out of space
+//
+
+#include <assert.h>
+#include <stdlib.h>
+#include <time.h>
+#include "mem.h"
+
+#define COALESCE_NONE 0
+#define COALESCE_ALL 1
+#define COALESCE_LOCAL 2
+
+int main() {
+    assert(Mem_Init(10000) == 0);
+    void* ptr;
+    srandom(time(NULL));
+    while(Mem_Alloc((long) random() % 50 + 1));
+    assert(m_error == E_NO_SPACE);
+    Mem_Dump();
+    exit(EXIT_SUCCESS);
+}
