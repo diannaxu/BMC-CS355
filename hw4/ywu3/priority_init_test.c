c6dc2ae08dc37ca8a8a817b7fe2769c73e7b48d4
yang-ng <131299315+yang-ng@users.noreply.github.com>
2024-03-09 00:18:14 -0500

    Update priority_init_test.c

--- hw4/ywu3/priority_init_test.c
+++ hw4/ywu3/priority_init_test.c
@@ -4,17 +4,17 @@
 
 int main() {
     if (thread_libinit(PRIORITY) == 0) {
-        printf("priority initialized successfully\n");
+        printf("PRIORITY initialized successfully.\n");
 
         if (thread_libterminate() == 0) {
-            printf("terminated successfully\n");
+            printf("terminated successfully.\n");
             exit(EXIT_SUCCESS);
         } else {
             fprintf(stderr, "error: failed to terminate thread library\n");
             exit(EXIT_FAILURE);
         }
     } else {
-        fprintf(stderr, "error: failed to initialize FIFO\n");
+        fprintf(stderr, "error: failed to initialize PRIORITY\n");
         exit(EXIT_FAILURE);
     }
 }
