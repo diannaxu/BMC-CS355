4a374513f427b7ee882624b3e9e613eec4015923
Kai Britt <116471255+KaiBritt@users.noreply.github.com>
2024-03-31 13:42:48 -0400

    update test

--- hw5/kbritt1/simple_alloc_free.c
+++ hw5/kbritt1/simple_alloc_free.c
@@ -4,7 +4,7 @@
 int main(){
     Mem_Init(4096);
     void * temp = Mem_Alloc(8);
-    free(temp);
+    Mem_Free(temp,0);
     Mem_Dump(); // should excpect to see two headers both are free their size should sum up to HS + HS + size1 + size2 = 4096
     return EXIT_SUCCESS;
 }
