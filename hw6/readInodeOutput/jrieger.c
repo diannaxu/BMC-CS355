884a4f547b96a0dfe2e432221b3a525616953570
jvrieger <57420053+jvrieger@users.noreply.github.com>
2024-04-02 22:06:17 -0400

    Create jrieger.c

---
+++ hw6/readInodeOutput/jrieger.c
@@ -0,0 +1,760 @@
+Inode region starts at byte offset: 0
+  
+Inode 0:
+Next inode: 1953460066
+Protect: 1953460066
+Number of links: 1953460066
+Size: 1953460066
+Owner's user ID: 1953460066
+Owner's group ID: 1953460066
+Change time: 1953460066
+Modification time: 1953460066
+Access time: 1953460066
+
+Inode 1:
+Next inode: 1953460066
+Protect: 1953460066
+Number of links: 1953460066
+Size: 1953460066
+Owner's user ID: 1953460066
+Owner's group ID: 1953460066
+Change time: 1953460066
+Modification time: 1953460066
+Access time: 1953460066
+
+Inode 2:
+Next inode: 1953460066
+Protect: 1953460066
+Number of links: 1953460066
+Size: 1953460066
+Owner's user ID: 1953460066
+Owner's group ID: 1953460066
+Change time: 1953460066
+Modification time: 1953460066
+Access time: 1953460066
+
+Inode 3:
+Next inode: 1953460066
+Protect: 1953460066
+Number of links: 1953460066
+Size: 1953460066
+Owner's user ID: 1953460066
+Owner's group ID: 1953460066
+Change time: 1953460066
+Modification time: 1953460066
+Access time: 1953460066
+
+Inode 4:
+Next inode: 1953460066
+Protect: 1953460066
+Number of links: 1953460066
+Size: 1953460066
+Owner's user ID: 1953460066
+Owner's group ID: 1953460066
+Change time: 1953460066
+Modification time: 1953460066
+Access time: 1953460066
+
+Inode 5:
+Next inode: 1953460066
+Protect: 1953460066
+Number of links: 1953460066
+Size: 512
+Owner's user ID: 0
+Owner's group ID: 4
+Change time: 10239
+Modification time: 14
+Access time: 10133
+
+Inode 12:
+Next inode: 1082
+Protect: 9196
+Number of links: 4895
+Size: 0
+Owner's user ID: 0
+Owner's group ID: 0
+Change time: 0
+Modification time: 0
+Access time: 1
+
+Inode 24:
+Next inode: 216
+Protect: 8872
+Number of links: 9470
+Size: 1494
+Owner's user ID: 0
+Owner's group ID: 0
+Change time: 9
+Modification time: 0
+Access time: 0
+
+Inode 29:
+Next inode: 1711
+Protect: 8200
+Number of links: 1674
+Size: 7023
+Owner's user ID: 0
+Owner's group ID: 0
+Change time: 0
+Modification time: 0
+Access time: 1
+
+Inode 30:
+Next inode: 10017
+Protect: 8285
+Number of links: 3724
+Size: 0
+Owner's user ID: 0
+Owner's group ID: 0
+Change time: 0
+Modification time: 0
+Access time: 0
+
+Inode 31:
+Next inode: 248848215
+Protect: 1082613012
+Number of links: 1271153997
+Size: 983041750
+Owner's user ID: 817945703
+Owner's group ID: -1399416449
+Change time: 1129466748
+Modification time: 1445266175
+Access time: 912587298
+
+Inode 32:
+Next inode: -2114113133
+Protect: -1900965221
+Number of links: 1122535529
+Size: 1438066232
+Owner's user ID: -1456716810
+Owner's group ID: -1807565781
+Change time: 1786372954
+Modification time: 1440297410
+Access time: -36252062
+
+Inode 33:
+Next inode: -609618428
+Protect: 1380766782
+Number of links: 796216733
+Size: 1011405703
+Owner's user ID: 1951351039
+Owner's group ID: -362708349
+Change time: 491399722
+Modification time: -823915318
+Access time: 1084853506
+
+Inode 34:
+Next inode: 1988550942
+Protect: -1783979008
+Number of links: 1105377736
+Size: 646998295
+Owner's user ID: 772792084
+Owner's group ID: -1128030899
+Change time: -1111763810
+Modification time: -1058100062
+Access time: -1925811059
+
+Inode 36:
+Next inode: -1792297049
+Protect: -973744795
+Number of links: 1804100384
+Size: 245150444
+Owner's user ID: 676187237
+Owner's group ID: -1606793180
+Change time: 342544896
+Modification time: 595060860
+Access time: 96052127
+
+Inode 40:
+Next inode: 1973925934
+Protect: -864033036
+Number of links: 520671755
+Size: -1359047245
+Owner's user ID: -1778316380
+Owner's group ID: 2790423
+Change time: -1710478433
+Modification time: 2025500234
+Access time: 1525506406
+
+Inode 44:
+Next inode: -1035413167
+Protect: -1488652488
+Number of links: 2048609565
+Size: 755520056
+Owner's user ID: 1563340960
+Owner's group ID: 301247960
+Change time: 389341725
+Modification time: -1975176391
+Access time: 1162645005
+
+Inode 46:
+Next inode: -2071167139
+Protect: -1677585170
+Number of links: 2
+Size: -1774308203
+Owner's user ID: -668834094
+Owner's group ID: 1425141254
+Change time: -1472812106
+Modification time: 959578588
+Access time: 1857994368
+
+Inode 49:
+Next inode: 41590705
+Protect: -1687437773
+Number of links: 1542865197
+Size: -317584347
+Owner's user ID: 808989991
+Owner's group ID: 1255280897
+Change time: 2033269617
+Modification time: 527343981
+Access time: 1931601984
+
+Inode 50:
+Next inode: 1577000704
+Protect: -2033487566
+Number of links: 726692290
+Size: -859991679
+Owner's user ID: -1936791147
+Owner's group ID: -350800855
+Change time: -1391036194
+Modification time: 556683553
+Access time: -1568789136
+
+Inode 52:
+Next inode: 269223944
+Protect: 269159179
+Number of links: 218959380
+Size: 303567886
+Owner's user ID: 303239955
+Owner's group ID: 336729112
+Change time: 303305748
+Modification time: 505091092
+Access time: 337321502
+
+Inode 53:
+Next inode: 269158921
+Protect: 17039616
+Number of links: 33817091
+Size: 134612741
+Owner's user ID: 856425221
+Owner's group ID: 285343745
+Change time: 304153603
+Modification time: 1363215665
+Access time: 1898058593
+
+Inode 54:
+Next inode: -186329916
+Protect: -708463195
+Number of links: 1716975077
+Size: -1500084618
+Owner's user ID: -422132042
+Owner's group ID: 1464285174
+Change time: -1752729753
+Modification time: -674777177
+Access time: -620759065
+
+Inode 55:
+Next inode: -989790260
+Protect: -713050447
+Number of links: 939446572
+Size: -86132162
+Owner's user ID: -1936232868
+Owner's group ID: 145464810
+Change time: -34998366
+Modification time: 1442962534
+Access time: -956235984
+
+Inode 57:
+Next inode: -943935052
+Protect: 1761390153
+Number of links: 1108170038
+Size: 1184903554
+Owner's user ID: 104486344
+Owner's group ID: 748154474
+Change time: -1278451944
+Modification time: 1953172159
+Access time: 70985915
+
+Inode 58:
+Next inode: 903148254
+Protect: -29981439
+Number of links: 490777482
+Size: -1775357800
+Owner's user ID: 1828967675
+Owner's group ID: 1720754115
+Change time: -68804642
+Modification time: -30598880
+Access time: 221446668
+
+Inode 61:
+Next inode: -904001162
+Protect: 2110589124
+Number of links: 157826726
+Size: -206153299
+Owner's user ID: -1932767934
+Owner's group ID: 501167128
+Change time: -1795921411
+Modification time: -1377544393
+Access time: 611779168
+
+Inode 62:
+Next inode: 1775309562
+Protect: -1472422107
+Number of links: 1927984273
+Size: 427295070
+Owner's user ID: -28401908
+Owner's group ID: -215010180
+Change time: -1861812360
+Modification time: -1899642476
+Access time: -117991469
+
+Inode 63:
+Next inode: 2146379007
+Protect: -745853277
+Number of links: 374681548
+Size: 1799398250
+Owner's user ID: -2026847310
+Owner's group ID: 1306742576
+Change time: 951098196
+Modification time: -1523674970
+Access time: 1461984948
+
+Inode 65:
+Next inode: 244640679
+Protect: 2089737776
+Number of links: 181213974
+Size: -1692246693
+Owner's user ID: -176978804
+Owner's group ID: -272580510
+Change time: -230679349
+Modification time: -2106365989
+Access time: -2104490158
+
+Inode 66:
+Next inode: 2044730676
+Protect: 1307002017
+Number of links: 279251672
+Size: 296488152
+Owner's user ID: 349337457
+Owner's group ID: -463604381
+Change time: 2046926333
+Modification time: -1936940968
+Access time: 1577414077
+
+Inode 69:
+Next inode: -465801850
+Protect: -197791934
+Number of links: 928301538
+Size: -835303329
+Owner's user ID: -580700175
+Owner's group ID: 1943098942
+Change time: -494006217
+Modification time: -1536090594
+Access time: -829903477
+
+Inode 70:
+Next inode: -626513115
+Protect: -152167526
+Number of links: 1752585274
+Size: 2080070120
+Owner's user ID: 756951569
+Owner's group ID: 1942745047
+Change time: -1934920047
+Modification time: 580853245
+Access time: -470005944
+
+Inode 73:
+Next inode: -506642853
+Protect: 1658415915
+Number of links: 1623664838
+Size: 912236697
+Owner's user ID: 1950884161
+Owner's group ID: 155341571
+Change time: 1391061264
+Modification time: 949618653
+Access time: 1897490501
+
+Inode 76:
+Next inode: 975229297
+Protect: 1685116290
+Number of links: 212863330
+Size: -1527166905
+Owner's user ID: 1039167617
+Owner's group ID: -588794374
+Change time: 1152713872
+Modification time: -1843442143
+Access time: -1563672800
+
+Inode 77:
+Next inode: -1003551107
+Protect: -1173723103
+Number of links: 1614926035
+Size: 2093330772
+Owner's user ID: -1806921243
+Owner's group ID: 1275913737
+Change time: -73699876
+Modification time: -696317607
+Access time: 832286223
+
+Inode 79:
+Next inode: 74369485
+Protect: 1993420374
+Number of links: 1421112159
+Size: 733026773
+Owner's user ID: 1570532852
+Owner's group ID: -1077272982
+Change time: 141134395
+Modification time: 1643500436
+Access time: -228236388
+
+Inode 80:
+Next inode: -1863257933
+Protect: 1973586104
+Number of links: 1486282700
+Size: -1203075933
+Owner's user ID: 1856626174
+Owner's group ID: -207156711
+Change time: -1496431555
+Modification time: -344048584
+Access time: -461672916
+
+Inode 81:
+Next inode: -247498693
+Protect: 1075537193
+Number of links: 1687927011
+Size: 1005670965
+Owner's user ID: -2129868966
+Owner's group ID: -1039932786
+Change time: -674034265
+Modification time: -1097812350
+Access time: 1487916846
+
+Inode 82:
+Next inode: 787076302
+Protect: 1276400218
+Number of links: 192142254
+Size: -1857463549
+Owner's user ID: -1919807032
+Owner's group ID: 1348437816
+Change time: 992374064
+Modification time: -579604465
+Access time: 504126660
+
+Inode 83:
+Next inode: -2004702212
+Protect: 239923204
+Number of links: 282727151
+Size: -1445223833
+Owner's user ID: 944079028
+Owner's group ID: 2082360122
+Change time: 950282648
+Modification time: 1951162231
+Access time: 251448842
+
+Inode 85:
+Next inode: 851501177
+Protect: -44308741
+Number of links: 1888880005
+Size: -1332213606
+Owner's user ID: -1058485952
+Owner's group ID: 1011079774
+Change time: 875172888
+Modification time: -1756107490
+Access time: -926346291
+
+Inode 86:
+Next inode: 1442853275
+Protect: 1467305857
+Number of links: 880687123
+Size: 814502752
+Owner's user ID: -423807982
+Owner's group ID: 1656497247
+Change time: 290349821
+Modification time: 998645407
+Access time: -795769265
+
+Inode 87:
+Next inode: -570288155
+Protect: 10
+Number of links: 1262996368
+Size: 1421260728
+Owner's user ID: -939536440
+Owner's group ID: -976069977
+Change time: 1638807968
+Modification time: -1314702644
+Access time: 294626897
+
+Inode 88:
+Next inode: -1412342280
+Protect: -1367209799
+Number of links: 369903220
+Size: -1152878146
+Owner's user ID: -2106021188
+Owner's group ID: -989478499
+Change time: -1731750074
+Modification time: 1091939145
+Access time: -1024661752
+
+Inode 89:
+Next inode: -1111260988
+Protect: 1689035665
+Number of links: 1789152691
+Size: 831122978
+Owner's user ID: -1190111224
+Owner's group ID: -508884370
+Change time: -6756512
+Modification time: -212499921
+Access time: -1817110526
+
+Inode 91:
+Next inode: -1359499509
+Protect: -838264835
+Number of links: 2141699057
+Size: -112284613
+Owner's user ID: -444300941
+Owner's group ID: 132268928
+Change time: -270577371
+Modification time: 302999302
+Access time: 348129047
+
+Inode 93:
+Next inode: 503865118
+Protect: -2101862653
+Number of links: 811560336
+Size: 990279917
+Owner's user ID: 1677207973
+Owner's group ID: 1789047642
+Change time: 119839273
+Modification time: 1390802484
+Access time: 1500573013
+
+Inode 95:
+Next inode: -593519321
+Protect: 949160098
+Number of links: 183118964
+Size: -1008959949
+Owner's user ID: 1806655602
+Owner's group ID: 246354271
+Change time: -1950664788
+Modification time: -1034552421
+Access time: 1000273048
+
+Inode 96:
+Next inode: 1362822608
+Protect: 1554971012
+Number of links: 552294667
+Size: -1974824259
+Owner's user ID: 200750430
+Owner's group ID: 1133813611
+Change time: -2035183291
+Modification time: 244757054
+Access time: 1012912567
+
+Inode 97:
+Next inode: -1868785160
+Protect: -163740505
+Number of links: 1488674075
+Size: -1361354500
+Owner's user ID: -905713622
+Owner's group ID: -873252090
+Change time: -569877660
+Modification time: 12
+Access time: -909618910
+
+Inode 98:
+Next inode: -1095796952
+Protect: -349186274
+Number of links: 1405396748
+Size: -85544465
+Owner's user ID: 740633211
+Owner's group ID: 340635389
+Change time: 1354956751
+Modification time: 876075788
+Access time: 1911744082
+
+Inode 99:
+Next inode: -1157101153
+Protect: -676814481
+Number of links: 241951468
+Size: 263596437
+Owner's user ID: 558871077
+Owner's group ID: -738631596
+Change time: -2078998922
+Modification time: 1118557347
+Access time: 1308472542
+
+Inode 100:
+Next inode: -485891780
+Protect: 608447999
+Number of links: 1258390398
+Size: -84677438
+Owner's user ID: 2016013752
+Owner's group ID: 268563389
+Change time: 479736218
+Modification time: -1860326315
+Access time: -1921767794
+
+Inode 101:
+Next inode: -1859681279
+Protect: 1002468567
+Number of links: 530335673
+Size: -979715938
+Owner's user ID: -370293857
+Owner's group ID: -1790677832
+Change time: -1786692397
+Modification time: 1634521184
+Access time: 754094932
+
+Inode 102:
+Next inode: 148866881
+Protect: -1752523183
+Number of links: 555443898
+Size: 742364549
+Owner's user ID: -1365776229
+Owner's group ID: 127739825
+Change time: -897794756
+Modification time: 2072255034
+Access time: -226270303
+
+Inode 104:
+Next inode: 1390865533
+Protect: 1939116267
+Number of links: 1377306081
+Size: -485045602
+Owner's user ID: -304434616
+Owner's group ID: -1568215024
+Change time: 2063065450
+Modification time: -1265743050
+Access time: 587625527
+
+Inode 107:
+Next inode: -1247976541
+Protect: 1651976089
+Number of links: 1882174670
+Size: 650883918
+Owner's user ID: 801392512
+Owner's group ID: -1568266791
+Change time: -2135113395
+Modification time: 1700407490
+Access time: 605812875
+
+Inode 108:
+Next inode: 438931804
+Protect: 406862528
+Number of links: 1340100613
+Size: 1061024376
+Owner's user ID: -1754915856
+Owner's group ID: -2079411862
+Change time: -94182780
+Modification time: -1800985801
+Access time: 1974390965
+
+Inode 109:
+Next inode: 813436640
+Protect: -1595887223
+Number of links: 1668211164
+Size: -1476960823
+Owner's user ID: -1906500367
+Owner's group ID: -1336531763
+Change time: 1268121230
+Modification time: -582449156
+Access time: 504218773
+
+Inode 112:
+Next inode: 1571527003
+Protect: -229853330
+Number of links: 1544578979
+Size: -504744458
+Owner's user ID: 443594234
+Owner's group ID: 254446310
+Change time: -817081191
+Modification time: 817110997
+Access time: 646799288
+
+Inode 113:
+Next inode: 1983019074
+Protect: 1644693997
+Number of links: 1921774329
+Size: 1274957392
+Owner's user ID: 507000507
+Owner's group ID: -845812502
+Change time: -884942695
+Modification time: 1365006607
+Access time: -1077440814
+
+Inode 114:
+Next inode: -1465258050
+Protect: -326257428
+Number of links: 388311067
+Size: 1922335128
+Owner's user ID: 1267542990
+Owner's group ID: 1700002471
+Change time: -2131576824
+Modification time: -164632520
+Access time: -1012999977
+
+Inode 115:
+Next inode: 1285162437
+Protect: 774459248
+Number of links: 1668033635
+Size: -699805281
+Owner's user ID: 847847106
+Owner's group ID: 1107882081
+Change time: 1603869443
+Modification time: -181341136
+Access time: 1765919481
+
+Inode 116:
+Next inode: -1810919652
+Protect: 1793236200
+Number of links: 1664469301
+Size: -48846726
+Owner's user ID: -361803257
+Owner's group ID: -1320812938
+Change time: -1714613343
+Modification time: -665721412
+Access time: 1550671988
+
+Inode 119:
+Next inode: -1003939747
+Protect: -383537892
+Number of links: 177463617
+Size: 1726333402
+Owner's user ID: -1192113440
+Owner's group ID: -1642758444
+Change time: 1378141944
+Modification time: -579999873
+Access time: 463575039
+
+Inode 121:
+Next inode: -572718316
+Protect: 1896362403
+Number of links: 704040474
+Size: 1678341306
+Owner's user ID: 204711676
+Owner's group ID: -1484902659
+Change time: -1022166818
+Modification time: -65193753
+Access time: -556139717
+
+Inode 122:
+Next inode: -637984740
+Protect: 911338734
+Number of links: 1906146989
+Size: -1637604754
+Owner's user ID: -1912183188
+Owner's group ID: -54554822
+Change time: -844547533
+Modification time: -96506658
+Access time: 2077505676
+
+Inode 123:
+Next inode: 961490393
+Protect: -1022674140
+Number of links: 678410966
+Size: 431908052
+Owner's user ID: -1010578019
+Owner's group ID: -62672606
+Change time: 445950428
+Modification time: 1234046320
+Access time: 1602477371
